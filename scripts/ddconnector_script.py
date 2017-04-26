4308620f91b7211139df6df00af38644b591b426
aidan <erhuabushuo@gmail.com>
2017-04-26 11:52:58 +0800

    aiohttp无效

--- scripts/ddconnector_script.py
+++ scripts/ddconnector_script.py
@@ -9,7 +9,7 @@ def main():
     logging.basicConfig(filename="ddconnector.log", 
                         level=logging.INFO,
                         format='%(asctime)s %(name)-4s %(levelname)-4s %(message)s')
-    client = Client('aiohttp+https://ca252a631c4b437cac81ea0ad3e545ff:d31c83bc53b644138831c7c7d41ba661@sdlog.doordu.com:8205/17')
+    client = Client('https://ca252a631c4b437cac81ea0ad3e545ff:d31c83bc53b644138831c7c7d41ba661@sdlog.doordu.com:8205/17')
     config = configparser.ConfigParser()
     config.read('/etc/ddconnector.ini')
     server = Server(config, client)
