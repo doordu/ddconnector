996e881c59b477c67640f16113f6b4dd8c774515
aidan <erhuabushuo@gmail.com>
2017-05-03 15:17:51 +0800

    支持手动指明配置文件路径，这样单台可跑多个实例

--- scripts/ddconnector_script.py
+++ scripts/ddconnector_script.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 from logging.config import fileConfig
 import configparser
@@ -7,11 +8,16 @@ from raven import Client
 from raven_aiohttp import AioHttpTransport
 
 def main():
-    fileConfig('/etc/ddconnector.ini')
+    config_file = '/etc/ddconnector.ini'
+    
+    if len(sys.argv) > 1:
+        config_file = sys.argv[1]
+    
+    fileConfig(config_file)
     
     client = Client('https://ca252a631c4b437cac81ea0ad3e545ff:d31c83bc53b644138831c7c7d41ba661@sdlog.doordu.com:8205/17', transport=AioHttpTransport)
     config = configparser.ConfigParser()
-    config.read('/etc/ddconnector.ini')
+    config.read(config_file)
     server = Server(config, client)
     server.run()
     
