0fa6b4a07ef05b36803c3e309513d121a4b50afd
aidan <erhuabushuo@gmail.com>
2017-05-08 13:00:32 +0800

    加入gc前后状态

--- ddconnector/processors/gc.py
+++ ddconnector/processors/gc.py
@@ -11,14 +11,24 @@ def gc(protocol, msg):
     garbage collection
     """
     now = time.time()
-    for p, t in protocol.server.transports:
-        if now - t > EXPIRED_DURATION:
+    before_gc_total = len(protocol.server.transports)
+    
+    for guid, (transport, last_ping) in protocol.server.transports.items():
+        if now - last_ping > EXPIRED_DURATION:
             try:
-                p.close() 
+                transport.close() 
             except Exception:
                 pass
+            
+            try:
+                del protocol.server.transports['guid']
+            except KeyError:
+                pass
+            
+    after_gc_total = len(protocol.server.transports)
     
-    response = "垃圾回收结束！"    
+    
+    response = "垃圾回收结束！{0:d} -> {1:d}".format(before_gc_total, after_gc_total)    
     protocol.transport.write(response.encode("utf-8"))
     protocol.transport.close()
     
