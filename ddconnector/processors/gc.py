d34031c345f30eaea30ed0ebdb725a3060e8437d
aidan <erhuabushuo@gmail.com>
2017-05-09 22:10:05 +0800

    准备升级

--- ddconnector/processors/gc.py
+++ ddconnector/processors/gc.py
@@ -12,16 +12,13 @@ def gc(protocol, msg):
     garbage collection
     """
     now = time.time()
-    before_gc_total = len(protocol.server.doors)
     
     for p in protocol.server.doors.values():
-        if self.last_time != 0 and now - p.last_time > EXPIRED_SECONDS:
+        if p.last_time != 0 and now - p.last_time > EXPIRED_SECONDS:
             p.transport.close()
             
-    after_gc_total = len(protocol.server.doors)
     
-    
-    response = "垃圾回收结束！{0:d} -> {1:d}".format(before_gc_total, after_gc_total)    
+    response = "垃圾回收结束！"    
     protocol.transport.write(response.encode("utf-8"))
     protocol.transport.close()
     
