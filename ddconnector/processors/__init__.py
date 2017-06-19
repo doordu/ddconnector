05a88bc5ffafcb93cb0c4c2782a9ed5ccf9d3d61
jipeng <jipeng@doordu.com>
2017-06-19 15:19:38 +0800

    增加防拆报警功能

--- ddconnector/processors/__init__.py
+++ ddconnector/processors/__init__.py
@@ -1,6 +1,7 @@
 from .heartbeat import heartbeat
 from .opendoor import opendoor
 from .cardnew import cardnew
+from .alert import alert
 from .debug import debug
 from .magic import magic
 from .total import total
@@ -10,6 +11,7 @@ processors = {
     'heart_beat': heartbeat,                        # 心跳
     'open_door': opendoor,                          # 开门
     'cardNew': cardnew,                             # 黑白名单
+    'alert': alert,                                 # 防拆报警
     'debug': debug,                                 # 日志
     'total': total,                                 # 统计连接数
     'gc': gc,                                       # 连接回收
