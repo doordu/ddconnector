290cb5491a42357b2344a90e4b41b3732c47a87d
jipeng <jipeng@doordu.com>
2017-06-21 13:57:00 +0800

    解决防拆报警问题

--- ddconnector/processors/alert.py
+++ ddconnector/processors/alert.py
@@ -32,17 +32,17 @@ async def get_access_token(protocol):
                     pass
             
                 
-async def alert_to_service(protocol, guid):
+async def alert_to_service(protocol, alert_time, guid):
     global ACCESS_TOKEN
     alert_url = urljoin(protocol.server.config['ddservice']['host'], '/dds/door/v1/message/alert')
     #logging.info("alert_url：%s ", alert_url)
-    alert_params = {"guid": guid, "access_token": ACCESS_TOKEN,"timestamp":int(time.time())}
+    alert_params = {"guid": guid, "access_token": ACCESS_TOKEN,"timestamp":alert_time}
     #logging.info("alert_params：%s ", alert_params)
     async with aiohttp.ClientSession() as session:
         async with session.post(alert_url, params=alert_params, timeout=10) as resp:
             #logging.info("发送防拆报警到服务，状态码：%s ", resp.status)
             result = await resp.json()
-            #logging.info("发送防拆报警到服务，结果：%s ", await resp.text())
+            logging.info("发送防拆报警到服务，结果：%s ", await resp.text())
             if resp.status == 400 and result['code'] == 10501:
                 # ACCESS_TOKEN过期
                 ACCESS_TOKEN = None
@@ -53,13 +53,13 @@ async def alert_to_service(protocol, guid):
 
 async def alert(protocol, msg):
     guid = msg['guid']
-    alert_time = time.time()
+    alert_time = int(time.time())
     if alert_time - protocol.alert_time > ALERT_FREQUENTLY:
         logging.info("收到报警信息！guid: %s，达到上报时间", guid)
         # 超过60s报警
         protocol.alert_time = alert_time
         await get_access_token(protocol)
-        await alert_to_service(protocol, guid)
+        await alert_to_service(protocol, alert_time, guid)
     else:
         logging.info("收到报警信息！guid: %s，未达到上报时间", guid)
 
